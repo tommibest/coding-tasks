41c8baa1c2fe3f0046e78e0cb81ef81fc1e33137
Tomasz Zaras <tomasz.zaras@grandparade.co.uk>
2019-06-28 20:30:22 +0200

    PondSize calculation, improved recursion.

--- src/main/java/com/zaras/pondsize/PondSize.java
+++ src/main/java/com/zaras/pondsize/PondSize.java
@@ -8,28 +8,26 @@ public class PondSize {
 
     public static void main(String[] args) {
         int[][] area = new int[][]{
-            {0, 0, 2, 0},
-            {0, 1, 0, 1},
-            {1, 1, 1, 1},
-            {0, 0, 0, 1}
+            {0, 0, 1, 2, 0},
+            {0, 1, 1, 0, 1},
+            {1, 1, 1, 1, 1},
+            {0, 0, 0, 2, 1}
         };
 
         PondSize pondSize = new PondSize();
-        List<Integer> expected = Arrays.asList(1, 2, 4);
+        List<Integer> expected = Arrays.asList(2, 3, 3);
         List<Integer> result = pondSize.calculate(area);
         System.out.println(result);
         System.out.println(result.containsAll(expected));
     }
 
     private List<Integer> calculate(int[][] area) {
-        List<Integer> results = new LinkedList<>();
-        int sizeX = area[0].length;
-        int sizeY = area.length;
-        for (int y = 0; y < sizeY; y++) {
-            for (int x = 0; x < sizeX; x++) {
+        LinkedList<Integer> results = new LinkedList<>();
+        for (int x = 0; x < area.length; x++) {
+            for (int y= 0; y < area[0].length; y++) {
                 int result = visit(area, x, y);
                 if (result != 0) {
-                    ((LinkedList<Integer>) results).addLast(result);
+                    results.addLast(result);
                 }
             }
         }
@@ -37,38 +35,17 @@ public class PondSize {
     }
 
     private int visit(int[][] area, int x, int y) {
-        int sizeX = area[0].length;
-        int sizeY = area.length;
-        int size = 0;
+        if (x < 0 || x >= area.length ||
+            y < 0 || y >= area[0].length ||
+            area[x][y] != 0) {
+            return 0;
+        }
 
-        if (area[y][x] == 0) {
-            area[y][x] = -1;
-
-            size++;
-
-            if ((x + 1 < sizeX) && (y - 1 >= 0)) {
-                size += visit(area, x + 1, y - 1);
-            }
-            if (x + 1 < sizeX) {
-                size += visit(area, x + 1, y);
-            }
-            if ((x + 1 < sizeX) && (y + 1 < sizeY)) {
-                size += visit(area, x + 1, y + 1);
-            }
-            if (y + 1 < sizeY) {
-                size += visit(area, x, y + 1);
-            }
-            if ((x - 1 >= 0) && (y + 1 < sizeY)) {
-                size += visit(area, x - 1, y + 1);
-            }
-            if (x - 1 >= 0) {
-                size += visit(area, x - 1, y);
-            }
-            if ((x - 1 >= 0) && (y - 1 >= 0)) {
-                size += visit(area, x - 1, y - 1);
-            }
-            if (y - 1 >= 0) {
-                size += visit(area, x, y - 1);
+        int size = 1;
+        area[x][y] = -1;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                size += visit(area, x + i, y + j);
             }
         }
         return size;
